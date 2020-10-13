832689d418676f626b7d7fe761bf6504992012d5
poyawang <poyawang12@gmail.com>
2020-10-14 04:11:16 +0800

    w4 commit

--- lesson4/js/preston.js
+++ lesson4/js/preston.js
@@ -4,6 +4,12 @@ function toggleMenu() {
 
 var theYear = new Date().getFullYear();
 document.getElementById('year').innerHTML = theYear;
-var time = document.lastModified;
-document.getElementById('datetime').innerHTML = time;
+// var time = document.lastModified;
+// document.getElementById('datetime').innerHTML = time;
 
+const dow = ['Sunday', 'Monday', 'Tuesday', 'wednesday', 'Thursday', 'Friday', 'Saturday']
+const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+let date = new Date();
+let output = dow[date.getDay()]+', '+ date.getDate() + ' ' + month[date.getMonth()] + ' '+date.getFullYear();
+document.getElementById('current-date').innerHTML = output;
