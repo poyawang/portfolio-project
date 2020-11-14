6e7cadb071e94a8eab4a79abe184a0e4d30525c4
poyawang <poyawang12@gmail.com>
2020-11-14 18:12:23 +0800

    w9 commit

---
+++ lesson9/js/homepage.js
@@ -0,0 +1,46 @@
+const url = 'https://byui-cit230.github.io/weather/data/towndata.json'
+const town1 = document.getElementById('town1')
+const town2 = document.getElementById('town2')
+const town3 = document.getElementById('town3')
+
+
+fetch(url)
+    .then(response => response.json())
+    .then(jsonObject => {
+        const towns = jsonObject['towns'];
+        let h1='';
+        h1 += `<h2>${towns[4].name}</h2>
+        <p class='motto'>${towns[4].motto}</p>
+        <P>Year Founded: ${towns[4].yearFounded}</P>
+        <P>Population: ${towns[4].currentPopulation}</P>
+        <P>Average Rainfall: ${towns[4].averageRainfall}</P>
+        <img src='../lesson9/img/town1.jpg' alt='${towns[4].photo}'>
+        `;
+        
+        town1.innerHTML = h1;
+        
+        
+        let h2= '';
+        h2 += `<h2>${towns[1].name}</h2>
+        <p class='motto'>${towns[1].motto}</p>
+        <P>Year Founded: ${towns[1].yearFounded}</P>
+        <P>Population: ${towns[1].currentPopulation}</P>
+        <P>Average Rainfall: ${towns[1].averageRainfall}</P>
+        <img src='../lesson9/img/town2.jpg' alt='${towns[1].photo}'>
+        `;
+
+        town2.innerHTML = h2;
+        
+
+        let h3 = '';
+        h3 += `<h2>${towns[5].name}</h2>
+        <p class='motto'>${towns[5].motto}</p>
+        <P>Year Founded: ${towns[5].yearFounded}</P>
+        <P>Population: ${towns[5].currentPopulation}</P>
+        <P>Average Rainfall: ${towns[5].averageRainfall}</P>
+        <img src='../lesson9/img/town3.jpg' alt='${towns[5].photo}'>
+        `;
+        
+        town3.innerHTML =h3;
+        
+    });
---
+++ lesson9/js/style.js
@@ -0,0 +1,14 @@
+// footer time 
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+var theYear = new Date().getFullYear();
+document.getElementById('year').innerHTML = theYear;
+
+const dow = ['Sunday', 'Monday', 'Tuesday', 'wednesday', 'Thursday', 'Friday', 'Saturday']
+const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+let date = new Date();
+let output = dow[date.getDay()]+', '+ date.getDate() + ' ' + month[date.getMonth()] + ' '+date.getFullYear();
+document.getElementById('current-date').innerHTML = output;
